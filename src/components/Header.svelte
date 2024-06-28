0fa880ea172a6261d3bc8a2a035d5bc4587e4ac8
JanKocanda <bun.noob.bun@gmail.com>
2024-06-28 21:36:57 +0200

    added header

---
+++ src/components/Header.svelte
@@ -0,0 +1,74 @@
+<script lang="ts">
+    import Menu from "./menu.svelte";
+
+    // import { onMount } from 'svelte';
+</script>
+
+<main class="header">
+    <h1 class="header-title">
+        only
+    </h1>
+    <p class="header-text">
+        hey<br>
+        look around and hit me up!
+    </p>
+    <Menu />
+</main>
+
+<style lang="scss">
+  $headerFolded: 4rem;
+  $headerFoldDelay: 2000ms;
+
+  @keyframes fold {
+    0% {
+      height: 100vh;
+    }
+    100% {
+      height: $headerFolded;
+      flex-direction: row;
+      justify-content: space-between;
+    }
+  }
+  @keyframes moveTitle {
+    0% {
+      font-size: var(--font-size-2xl);
+    }
+    100% {
+      font-size: var(--font-size-lg);
+      margin-right: auto;
+    }
+  }
+  @keyframes moveText {
+    100% {
+      transform: translateX(-500%);
+    }
+  }
+
+  .header {
+    position: fixed;
+    animation: fold 800ms forwards $headerFoldDelay;
+
+    background: var(--linearPrimarySecondary);
+    width: 100vw;
+    height: 100vh;
+    padding: 1rem;
+
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+
+    &-title {
+      font-size: var(--font-size-2xl);
+      animation: moveTitle 800ms forwards $headerFoldDelay;
+    }
+
+    &-text {
+      margin-top: 10rem;
+      position: absolute;
+      text-align: center;
+      font-size: var(--font-size-sm);
+      animation: moveText 800ms forwards $headerFoldDelay;
+    }
+  }
+</style>
---
+++ src/components/menu.svelte
@@ -0,0 +1,124 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+
+    let menuVisible: boolean = false;
+
+    let menuButton: HTMLElement;
+    let menu: HTMLElement;
+
+    onMount(main);
+
+    function main() {
+      if (menuButton)
+        menuButton.addEventListener('click', toggleMenu);
+
+      document.body.addEventListener('scroll', () => {
+        if (menuVisible)
+          toggleMenu();
+      });
+    }
+
+    function toggleMenu() {
+      if (menuButton) {
+        menuVisible = !menuVisible;
+        menuButton.classList.toggle('menu-button-active');
+      }
+    }
+</script>
+
+<main class="menu" bind:this={menu}>
+  <div bind:this={menuButton} class="menu-button {menuVisible ? 'menu-active' : ''}">
+    <div class="menu-button-line"></div>
+    <div class="menu-button-line"></div>
+    <div class="menu-button-line"></div>
+  </div>
+  {#if menuVisible}
+    <div class="menu-dropdown">
+      <ul>
+        <li>
+          <a href="#skills"># skills</a>
+        </li>
+        <li>
+          <a href="#projets"># projects</a>
+        </li>
+        <li>
+          <a href="#links"># links</a>
+        </li>
+      </ul>
+    </div>
+  {/if}
+</main>
+
+<style lang="scss">
+  @keyframes show {
+    0% {
+      opacity: 0;
+    }
+    100% {
+      opacity: 1;
+    }
+  }
+
+  .menu {
+    animation: show 800ms forwards 2000ms;
+    opacity: 0;
+
+    &-dropdown {
+      position: absolute;
+      top: 0;
+      right: 0;
+      margin-top: 4.6rem;
+      margin-right: 4.6rem;
+      padding: 1rem;
+      border: white 1px solid;
+      border-radius: var(--radius);
+      background: var(--secondary);
+      ul, ol {
+        list-style: none;
+        text-align: center;
+        li {
+          a {
+            color: var(--text-color);
+            text-decoration: none;
+            &:hover {
+              text-decoration: underline;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  .menu-button {
+    display: flex;
+    flex-direction: column;
+    gap: 0.4rem;
+    align-items: center;
+    margin-right: 3rem;
+    width: 2rem;
+    cursor: pointer;
+
+    &-line {
+      width: 100%;
+      height: 2px;
+      background: var(--text-color);
+      transition: 300ms;
+    }
+  }
+
+  .menu-active {
+    .menu-button-line:nth-child(1) {
+      transform-origin: center;
+      transform: rotate(45deg) translateY(0.8rem);
+    }
+    .menu-button-line:nth-child(2) {
+      transform-origin: left;
+      transform: scaleX(0);
+    }
+    .menu-button-line:nth-child(3) {
+      transform-origin: center;
+      transform: rotate(-45deg) translateY(-0.8rem);
+    }
+  }
+
+</style>
